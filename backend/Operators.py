19ec06794412fb7fe5743fa634dad83c0616d409
Aitor <107086354+Deanc001@users.noreply.github.com>
2024-10-25 03:56:52 -0500

    Operators.py

--- backend/Operators.py
+++ backend/Operators.py
@@ -1,1 +1,3 @@
-
+#No custom operator (%) in python so function def used instead. Function takes x and y checks if x is None, returns y, otherwise it returns x
+def null_coal(x,y):
+    return y if x is None else x
