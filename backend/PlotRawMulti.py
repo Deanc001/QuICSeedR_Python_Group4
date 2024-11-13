a902b627fa757ae63e14049e30e9c1a697e7d933
AntonLedoux <156055998+AntonLedoux@users.noreply.github.com>
2024-11-13 01:24:02 -0600

    Update PlotRawMulti.py

--- backend/PlotRawMulti.py
+++ backend/PlotRawMulti.py
@@ -1,42 +1,44 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-def PlotRawMulti(raw, samples, legend_position="upper left", xlim=None, ylim=None, custom_colors=None,
-                 xlab="Time (h)", ylab="Fluorescence", linetypes=None):
-    
-    time = raw.index.to_numpy(dtype=float)
-    
+
+def plot_raw_multi(raw, samples, legend_position="upper left",
+                   xlim=None, ylim=None, custom_colors=None,
+                   xlab="Time (h)", ylab="Fluorescence",
+                   linetypes=None, ax=None):
+
+    time = np.array(raw.index, dtype=float)
+
     if ylim is None:
-        ymax = raw.max().max()
+        ymax = np.nanmax(raw.values)
         ylim = (0, ymax / 0.8)
-        
+
     if xlim is None:
-        xlim = (time.min(), time.max())
-        
+        xlim = (np.nanmin(time), np.nanmax(time))
+
     if custom_colors is None:
-        colors_to_use = plt.cm.tab10(np.arange(len(samples)))
+        colors_to_use = plt.get_cmap("tab10")(np.linspace(0, 1, len(samples)))
     else:
-        colors_to_use = np.resize(custom_colors, len(samples))
-        
+        colors_to_use = (custom_colors * ((len(samples) // len(custom_colors)) + 1))[:len(samples)]
+
     if linetypes is None:
-        linetypes = ['solid'] * len(samples)
-    else: 
-        linetypes = np.resize(linetypes, len(samples))
-        
-    plt.figure(figsize=(8, 6))
-    plt.xlim(xlim)
-    plt.ylim(ylim)
-    plt.xlabel(xlab)
-    plt.ylabel(ylab)
-    
+        linetypes = ['-'] * len(samples)
+    else:
+        linetypes = (linetypes * ((len(samples) // len(linetypes)) + 1))[:len(samples)]
+
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(10, 6))
+
+    ax.set_xlabel(xlab)
+    ax.set_ylabel(ylab)
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+
     for i, sample in enumerate(samples):
-        sel = raw.columns.str.contains(f'^{sample}')
-        
-        for j in raw.columns[sel]:
-            valid_indices = ~raw[j].isna()
-            plt.plot(time[valid_indices], raw.loc[valid_indices, j], label=sample, color=colors_to_use[i],
-                     linestyle=linetypes[i])
-            
-    plt.legend(samples, loc=legend_position, fontsize=8)
-    plt.show()
+        sample_indices = [j for j, col_name in enumerate(raw.columns) if sample in col_name]
+        for j in sample_indices:
+            not_na_indices = ~raw.iloc[:, j].isna()
+            ax.plot(time[not_na_indices.values], raw.iloc[not_na_indices.values, j],
+                    color=colors_to_use[i], linestyle=linetypes[i])
+
+    ax.legend(samples, loc=legend_position, frameon=False)
