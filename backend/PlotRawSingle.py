651d388f4e9bc8f5c5d0f3e538f023bfbc3af468
AntonLedoux <156055998+AntonLedoux@users.noreply.github.com>
2024-11-13 01:32:58 -0600

    Update PlotRawSingle.py

--- backend/PlotRawSingle.py
+++ backend/PlotRawSingle.py
@@ -1,46 +1,48 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-def PlotRawSingle(raw, sample, legend_position="upper left", xlim=None, ylim=None, custom_colors=None,
-                 xlab="Time (h)", ylab="Fluorescence", linetypes=None):
-    
-    time = raw.index.to_numpy(dtype=float)
-    sel = raw.columns.str.contains(f'^{sample}')
-    
+
+def plot_raw_single(raw, sample, legend_position="upper left",
+                    xlim=None, ylim=None, custom_colors=None,
+                    xlab="Time (h)", ylab="Fluorescence",
+                    linetypes=None, ax=None):
+
+    time = np.array(raw.index, dtype=float)
+
+    sel = [j for j, col_name in enumerate(raw.columns) if col_name.startswith(sample)]
+
     if ylim is None:
-        ymax = raw.loc[:, sel].max().max()
+        ymax = np.nanmax(raw.iloc[:, sel].values)
         ylim = (0, ymax / 0.8)
-        
+
     if xlim is None:
-        xlim = (time.min(), time.max())
-        
+        xlim = (np.nanmin(time), np.nanmax(time))
+
     if custom_colors is None:
-        colors_to_use = plt.cm.tab10(np.arange(sel.sum()))
+        colors_to_use = plt.get_cmap("tab10")(np.linspace(0, 1, len(sel)))
     else:
-        colors_to_use = np.resize(custom_colors, sel.sum())
-        
+        colors_to_use = (custom_colors * ((len(sel) // len(custom_colors)) + 1))[:len(sel)]
+
     if linetypes is None:
-        linetypes = ['solid'] * sel.sum()
+        linetypes = ['-'] * len(sel)
     else:
-        linetypes = np.resize(linetypes, sel.sum())
-        
-    plt.figure(figsize=(8, 6))
-    plt.xlim(xlim)
-    plt.ylim(ylim)
-    plt.xlabel(xlab)
-    plt.ylabel(ylab)
-    
-    columns_sel = raw.columns[sel]
-    valid_indices = ~raw[columns_sel[0]].isna()
-    plt.plot(time[valid_indices], raw.loc[valid_indices, columns_sel[0]],
-             label=sample, color=colors_to_use[0], linestyle=linetypes[0])
-    
-    
-    for i in range(1, len(columns_sel)):
-        valid_indices = ~raw[columns_sel[i]].isna()
-        plt.plot(time[valid_indices], raw.loc[valid_indices, columns_sel[i]],
-                 label=f'{sample} {i+1}', color=colors_to_use[i], linestyle=linetypes[i])
-        
-    plt.legend(loc=legend_position, fontsize=8)
-    plt.show()
+        linetypes = (linetypes * ((len(sel) // len(linetypes)) + 1))[:len(sel)]
+
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(8, 6))
+
+    not_na_indices = ~raw.iloc[:, sel[0]].isna()
+    ax.plot(time[not_na_indices.values], raw.iloc[not_na_indices.values, sel[0]],
+            color=colors_to_use[0], linestyle=linetypes[0], label=f"1")
+
+    for i in range(1, len(sel)):
+        not_na_indices = ~raw.iloc[:, sel[i]].isna()
+        ax.plot(time[not_na_indices.values], raw.iloc[not_na_indices.values, sel[i]],
+                color=colors_to_use[i], linestyle=linetypes[i], label=f"{i + 1}")
+
+    ax.set_xlabel(xlab)
+    ax.set_ylabel(ylab)
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+
+    ax.legend(loc=legend_position, frameon=False)
