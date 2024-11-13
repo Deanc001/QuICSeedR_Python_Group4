34dff3817442dd51b4ae5f71e397fe0c1d819f10
AntonLedoux <156055998+AntonLedoux@users.noreply.github.com>
2024-11-12 22:55:52 -0600

    Update SpreadCalculation.py
    Updated

--- backend/SpreadCalculation.py
+++ backend/SpreadCalculation.py
@@ -1,21 +1,15 @@
-import pandas as pd
-
-def SpreadCalculation(calculation, id_col="content", rep_col="replicate", terms=['RAF', 'MPR', 'MS']):
-    
-    if not all(col in calculation.columns for col in [id_col, rep_col]):
-        raise ValueError("id_col and rep_col must be present in the calculation data frame")
-    
+def spread_calculation(calculation, id_col="content", rep_col="replicate", terms=None):
+    if id_col not in calculation.columns or rep_col not in calculation.columns:
+        raise ValueError("id_col and rep_col must be present in the calculation DataFrame.")
     if terms is None:
         terms = ['time_to_threshold', 'RAF', 'MPR', 'MS']
-        
     if not all(term in calculation.columns for term in terms):
-        raise ValueError("Not all specified terms are present in the calculation data frame")
-    
+        raise ValueError("Not all specified terms are present in the calculation DataFrame.")
+
     def spread_term(data, term):
-        
-        spread_df = data.pivot(index=rep_col, columns=id_col, values=term).reset_index(drop=True)
-        return spread_df
-    
+        pivoted = data[[id_col, rep_col, term]].pivot(index=rep_col, columns=id_col, values=term)
+        return pivoted.reset_index(drop=True)
+
     calculation_spread = {term: spread_term(calculation, term) for term in terms}
-    
+
     return calculation_spread
