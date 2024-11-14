90ee1ac2e83f15f673ced1c421ff995b03ddcd21
Aitor <107086354+Deanc001@users.noreply.github.com>
2024-11-14 13:45:23 -0600

    Optimized GetReplicate.py

--- backend/GetReplicate.py
+++ backend/GetReplicate.py
@@ -3,26 +3,20 @@ import numpy as np
 
 def GetReplicate(plate):
     replicate_counts = {} #dictionary to keep track of counts for each sample
-    #intialize empty data frame and fill with nan variables
-    replicate_data= pd.DataFrame(np.nan, index=plate.index, columns=plate.columns)
+    
+    #intialize empty data frame for storing replicate counts
+    replicate_data= pd.DataFrame(index=plate.index, columns=plate.columns)
 
-    #iterate through plate data by row and column
-    #loops through each cell in plate, updating replicate numbers in 'replicate data'
-    #if sample id is found, then it counts and assigns replicate num if Nan then it leaves as is
+   #Iterate over DataFrame Values to populate replicate counts
     for col in plate.columns:
         for row in plate.index:
-            sample_id = plate.loc[row,col]
+            sample_id = plate.at[row,col]  #Changed loc into at since only one value is accessed in data frame
             if pd.isna(sample_id): #if cell is empty, keep as Nan
-                replicate_data.loc[row,col]= np.nan
+                replicate_data.at[row,col]= np.nan  
             else:
-                sample_id = str(sample_id) #convert sample ID to string to be consistent
-                if sample_id not in replicate_counts:
-                    replicate_counts[sample_id] =1 #initialize count if sample ID is new
-                else:
-                    replicate_counts[sample_id] +=1 #increments if sample ID is repeat
-                
-                #assign current replicate number to the replicate_data DataFrame
-                replicate_data.loc[row,col]= replicate_counts[sample_id]
+                #start/increment the count for the sample_id
+                replicate_counts[sample_id] = replicate_counts.get(sample_id, 0) +1
+                replicate_data.at[row,col] = replicate_counts[sample_id] #assign the count
 
     return replicate_data
 
